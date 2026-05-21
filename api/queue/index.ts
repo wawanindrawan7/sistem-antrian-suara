5a66d94aa4b2f69ec3259dfd580c29bd598a96b0
Wawan Indrawan <37721680+wawanindrawan7@users.noreply.github.com>
2026-05-21 14:45:34 +0800

    update deploy server

--- api/queue/index.ts
+++ api/queue/index.ts
@@ -1,4 +1,4 @@
-import { handleQueueApiRequest } from "../src/server/queueApi.js";
+import { handleQueueApiRequest } from "../../src/server/queueApi.js";
 
 export default async function handler(req: any, res: any) {
   handleQueueApiRequest(req, res, "/");
