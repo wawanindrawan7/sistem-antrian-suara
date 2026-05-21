8eac9c65c51565c9a4b6ef9e1307aadb3412de43
Wawan Indrawan <37721680+wawanindrawan7@users.noreply.github.com>
2026-05-21 14:41:02 +0800

    update deploy server

--- api/queue/[[...route]].ts
+++
@@ -1,9 +0,0 @@
-import { handleQueueApiRequest } from "../../src/server/queueApi.js";
-
-export default function handler(req: any, res: any) {
-  const route = req.query?.route;
-  const segments = Array.isArray(route) ? route : route ? [route] : [];
-  const path = segments.length > 0 ? `/${segments.join("/")}` : "/";
-
-  handleQueueApiRequest(req, res, path);
-}
---
+++ api/queue/call.ts
@@ -0,0 +1,5 @@
+import { handleQueueApiRequest } from "../../src/server/queueApi.js";
+
+export default async function handler(req: any, res: any) {
+  handleQueueApiRequest(req, res, "/call");
+}
